61073546cfe121a906d15960930f9c6ba3ccff5f
ausbitbank <ausbitbank@gmail.com>
2021-04-04 03:44:50 +1000

    updates to downvoter, add voters and sort by payout time

--- src/pages/downvoter.vue
+++ src/pages/downvoter.vue
@@ -3,26 +3,27 @@
         <q-card class="text-center">
           <q-card-section>
             <p>Abuse fighting tool. Select a user, find and attempt to remove payouts.</p>
-            <p class="text-italic text-bold text-red">Alpha software, buggy, don't use if you don't understand the risks.</p>
+            <p class="text-italic text-bold text-red">Alpha, buggy, don't use if you don't understand the risks.</p>
             <q-input label="username" v-model="usernameField" />
             <q-btn label="Find pending payouts" @click="lookup(usernameField)" color="primary" bordered glossy class="q-ma-md" />
             <div v-if="loggedInUser">
               Downvote Power for {{ loggedInUser }}
-              <q-linear-progress dark stripe rounded size="20px" :value="this.downvotePowerPct / 100" color="red" class="q-mt-sm">
+              <q-linear-progress dark stripe rounded size="20px" :value="this.downvotePowerPct / 100" color="red">
                 <div class="absolute-full flex flex-center">
                     <q-badge color="black" text-color="primary" :label="this.downvotePowerPct" />
                 </div>
               </q-linear-progress>
-              <q-btn label="refresh" color="green" icon="refresh" class="q-ma-sm" />
+              <q-btn title="Refresh logged in account data" size="sm" round color="green" icon="refresh" class="q-ma-sm" />
             </div>
           </q-card-section>
           <q-card-section v-if="posts.length > 0">
             <h4>Pending payouts for {{ username }}</h4>
             <q-btn color="red" flat label="Downvote all" icon="thumb_down" @click="downvoteAll()" v-if="loggedInUser" />
             <q-list flat bordered separator>
-            <q-item v-for="post in posts.filter(p => p.net_rshares > 0)" :key="post.index">
+            <q-item v-for="post in postsWithPayoutNotVoted" :key="post.index">
               <q-item-section>
                 <router-link :to="returnPostLink(post.author, post.permlink)"><q-icon name="link" /> {{ post.permlink.substr(0,50) }}</router-link> Pays out ${{ post.payout }} {{ timeDelta(post.payout_at) }}
+                <q-table :data="post.active_votes" :columns="voteColumns" :pagination="{ sortBy: 'rshares', descending: true, rowsPerPage: 5 }" dense bordered separator="cell" />
                 <q-btn color="warning" icon="thumb_down" flat @click="vote(post.author, post.permlink, calcWeightForRshares(post.net_rshares))" v-if="loggedInUser" >&nbsp;&nbsp;&nbsp;Downvote {{ calcWeightForRshares(post.net_rshares) }} %</q-btn>
               </q-item-section>
             </q-item>
@@ -42,7 +43,21 @@ export default {
       username: '',
       posts: [],
       rewardFundPost: null,
-      median: null
+      median: null,
+      voteColumns: [
+        {
+          name: 'voter',
+          label: 'Voter',
+          field: 'voter',
+          sortable: true
+        },
+        {
+          name: 'rshares',
+          label: 'rshares',
+          field: 'rshares',
+          sortable: true
+        }
+      ]
     }
   },
   computed: {
@@ -80,9 +95,19 @@ export default {
       } else {
         return null
       }
+    },
+    postsWithPayoutNotVoted: function () {
+      if (this.posts.length > 0) {
+        return this.posts.filter(p => p.net_rshares > 0 && p.active_votes.filter(v => v.voter === this.loggedInUser).length === 0)
+      } else {
+        return []
+      }
     }
   },
   methods: {
+    sortPostsByAge () {
+      this.posts.sort((a, b) => { return moment(a.created) - moment(b.created) })
+    },
     downvoteAll () {
       this.posts.filter(p => p.net_rshares > 0 && p.active_votes.filter(v => v.voter === this.loggedInUser).length === 0).forEach(p => { this.downvotePost(p) })
     },
@@ -96,7 +121,6 @@ export default {
       var totalVests = vestingShares + receivedVestingShares - delegatedVestingShares
       var finalVests = totalVests * 1e6
       var weight = 10000
-      // var power = (this.account.voting_power * weight / 10000) / 500px
       var power = (this.downvotePower * weight / 10000) / 50
       var rshares = power * finalVests / 10000
       if (rsharesRequired < rshares) {
@@ -108,12 +132,13 @@ export default {
       }
     },
     async lookup (user) {
-      this.username = user
+      this.username = user.toLowerCase()
       this.posts = []
-      var params = { account: user, sort: 'payout' }
+      var params = { account: this.username, sort: 'payout' }
       this.$hive.api.callAsync('bridge.get_account_posts', params)
         .then(response => {
           this.posts = response
+          this.sortPostsByAge()
         })
     },
     async vote (author, permlink, weight) {
