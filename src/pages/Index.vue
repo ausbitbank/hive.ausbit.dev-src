8411e27ef12a7b143209cd067935348f7fcc7457
ausbitbank <ausbitbank@gmail.com>
2021-06-05 19:42:26 +1000

    Remove front page transition animations, remove "hot" carousel

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,23 +1,14 @@
 <template>
   <q-page class="flex flex-center">
     <div class="q-pa-md row items-start q-gutter-md content-start content-stretch self-stretch col-grow justify-center">
-      <transition appear enter-active-class="animated fadeInLeft" leave-active-class="animated fadeOutLeft">
-        <div style="width:400px" class="justify-center">
-          <block :blockNum="0" view="simple" class="q-mb-lg" />
-          <recent-posts-carousel account="ausbitbank" type="blog" />
-          <ranked-posts-carousel sortMethod="trending" />
-          <ranked-posts-carousel sortMethod="hot" />
-        </div>
-      </transition>
-      <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-        <nodes />
-      </transition>
-      <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-        <witnesses />
-      </transition>
-      <transition appear enter-active-class="animated fadeInRight" leave-active-class="animated fadeOutRight">
+      <div style="width:400px">
+        <block :blockNum="0" view="simple" class="q-mb-lg" />
+        <recent-posts-carousel account="ausbitbank" type="blog" style="width:400px" />
+        <ranked-posts-carousel sortMethod="trending" style="width:400px" />
+      </div>
         <coingecko />
-      </transition>
+        <nodes />
+        <witnesses />
     </div>
   </q-page>
 </template>
