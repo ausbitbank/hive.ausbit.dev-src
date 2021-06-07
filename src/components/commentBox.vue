396ae91b586bb5fa19a4bd15e3dc67a080f47cfc
ausbitbank <ausbitbank@gmail.com>
2021-06-07 22:39:21 +1000

    commentBox: add advanced options, minor visual tweaks, close on send, cancel button

--- src/components/commentBox.vue
+++ src/components/commentBox.vue
@@ -1,8 +1,18 @@
 <template>
-    <div flat bordered v-if="this.loggedInUser && !commentSent" style="margin:auto; min-width: 500px; max-width: 95%" class="q-ma-md q-pa-md rounded-borders">
+    <div flat bordered v-if="this.loggedInUser && !commentSent" style="" class="q-ma-md q-pa-sm rounded-borders">
         <q-card>
+        <div class="text-center">Reply to <router-link :to="returnUserLink(parent_author)">{{ parent_author }}</router-link> / <router-link :to="returnPostLink(parent_author, parent_permlink)">{{ parent_permlink.substr(0, 20) }}...</router-link></div>
         <vue-simplemde v-model="commentText" ref="markdownEditor" :configs="editorConfig" class="q-pa-sm" />
-        <q-btn @click="comment()" push color="primary" style="margin:auto">Reply to {{ parent_author }}</q-btn>
+        <q-expansion-item v-model="advanced" label="Advanced Options" icon="settings" color="grey">
+          <q-input label="Post Title (optional for comments)" v-model="title" />
+          <q-input label="JSON Metadata" v-model="defaultMeta" />
+          <q-input label="Percent HBD (0-10000)" v-model.number="percent_hbd" />
+          <q-input label="Max Accepted Payout" v-model="max_accepted_payout" />
+          <q-checkbox label="Allow Votes" v-model="allow_votes" :disable="true" />
+          <q-checkbox label="Allow Curation Rewards" v-model="allow_curation_rewards" :disable="true" />
+        </q-expansion-item>
+        <q-btn v-close-popup icon="comment" @click="comment()" push color="primary" style="margin:auto">Reply to {{ parent_author }}</q-btn>
+        <q-btn v-close-popup icon="close" label="Cancel" color="red" />
         </q-card>
     </div>
 </template>
@@ -21,11 +31,17 @@ export default {
       commentText: '',
       commentSent: false,
       title: '',
+      advanced: false,
+      defaultMeta: '{ "app": "ausbit/2021.06.07", "format": "markdown" }',
+      max_accepted_payout: '100000.000 HBD',
+      percent_hbd: 10000,
+      allow_votes: true,
+      allow_curation_rewards: true,
       editorConfig: {
         autosave: {
           enabled: false,
           uniqueId: 'ausbitCommentForm',
-          delay: 1000
+          delay: 0
         },
         autofocus: true,
         hideIcons: ['guide', 'side-by-side'],
@@ -36,42 +52,32 @@ export default {
   computed: {
     loggedInUser: {
       get () { return this.$store.state.hive.user.username }
-    }
+    },
+    loginType: { get () { return this.$store.state.hive.user.loginType } }
   },
   methods: {
     async comment () {
       var newpermlink = this.$hive.formatter.commentPermlink(this.parent_author, this.parent_permlink)
-      var metadata = { app: 'ausbit/2021.02.17' }
       var commentOptions = []
       if (this.$store.state.hive.user.settings.beneficiary.length !== 0) {
-        commentOptions = { author: this.loggedInUser, permlink: newpermlink, max_accepted_payout: '100000.000 HBD', percent_hbd: 10000, allow_votes: true, allow_curation_rewards: true, extensions: [[0, { beneficiaries: this.$store.state.hive.user.settings.beneficiary }]] }
+        commentOptions = { author: this.loggedInUser, permlink: newpermlink, max_accepted_payout: this.max_accepted_payout, percent_hbd: this.percent_hbd, allow_votes: this.allow_votes, allow_curation_rewards: this.allow_curation_rewards, extensions: [[0, { beneficiaries: this.$store.state.hive.user.settings.beneficiary }]] }
       } else {
-        commentOptions = { author: this.loggedInUser, permlink: newpermlink, max_accepted_payout: '100000.000 HBD', percent_hbd: 10000, allow_votes: true, allow_curation_rewards: true }
+        commentOptions = { author: this.loggedInUser, permlink: newpermlink, max_accepted_payout: this.max_accepted_payout, percent_hbd: this.percent_hbd, allow_votes: this.allow_votes, allow_curation_rewards: this.allow_curation_rewards }
       }
-      var op1 = ['comment', {
+      var op = ['comment', {
         parent_author: this.parent_author,
         parent_permlink: this.parent_permlink,
         author: this.loggedInUser,
         permlink: newpermlink,
         title: this.title,
         body: this.commentText,
-        json_metadata: metadata
+        json_metadata: this.defaultMeta,
+        comment_options: commentOptions
       }]
-      var op2 = ['comment_options', commentOptions]
-      var ops = [op1, op2]
-      this.$store.commit('hive/addToQueue', [this.loggedInUser, 'posting', ops])
-      /* const { success, msg, cancel, notInstalled, notActive } = await keychain(window, 'requestPost', this.loggedInUser, this.title, this.commentText, this.parent_permlink, this.parent_author, JSON.stringify(metadata), newpermlink, JSON.stringify(commentOptions))
-      if (success) { this.$q.notify('Comment Sent'); this.commentSent = true }
-      if (!cancel) {
-        if (notActive) {
-          console.error('Please allow Keychain to access this website')
-        } else if (notInstalled) { // alert('Please install Keychain')
-          console.error('Keychain not available')
-        } else {
-          console.info(msg)
-        }
-      } */
-    }
+      this.$store.commit('hive/addToQueue', [this.loggedInUser, 'posting', op])
+    },
+    returnUserLink (user) { return '/@' + user },
+    returnPostLink (author, permlink) { return '/@' + author + '/' + permlink }
   }
 }
 </script>
