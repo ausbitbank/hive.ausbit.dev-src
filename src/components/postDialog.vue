7f1ac7c414cb4638ee62415c13cefb6664a94187
ausbitbank <ausbitbank@gmail.com>
2021-06-18 11:02:40 +1000

    Add new postDialog.vue component

---
+++ src/components/postDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-card flat bordered style="max-width:900px; margin: auto" class="q-pa-md">
+    <div class="text-h5 text-center vertical-top"><router-link :to="returnPostPath(post.author, post.permlink)">{{ post.title }}</router-link></div>
+    <render :input="post.body" v-if="post.body" />
+    <render :input="summary" v-else />
+    <postFooter :post="post" />
+  </q-card>
+</template>
+<script>
+export default {
+  name: 'postDialog',
+  props: ['post'],
+  data () {
+    return {
+    }
+  },
+  components: {
+    postFooter: () => import('components/postFooter.vue'),
+    render: () => import('components/render.vue')
+  },
+  methods: {
+    returnPostPath (author, permlink) { return '/@' + author + '/' + permlink }
+  }
+}
+</script>
