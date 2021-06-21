4aa890d8867d1bb2a519e11ac098caae6d1f14fe
ausbitbank <ausbitbank@gmail.com>
2021-06-22 08:32:16 +1000

    propsEditor.vue : Allow editing json metadata, now uses queue (enabling smartlock login)

--- src/components/propsEditor.vue
+++ src/components/propsEditor.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <q-input outlined style="height: 100%" type="textarea" v-model="newjson" label="Edit your metadata directly here or use the tags below" />
-        <q-btn label="Submit changes" color="orange" icon="warning" @click="submitChanges()" v-if="newjson !== json && !edited" />
+        <q-btn flat label="Submit changes" color="orange" icon="warning" @click="submitChanges()" v-if="newjson !== json && !edited" />
         <div v-if="edited" class="text-caption">
             Your posting JSON metadata has been edited. Refresh the page in a moment to see the changes
         </div>
@@ -10,13 +10,13 @@
             Commonly Supported Props:
             <q-chip v-for="tag in commonTags" :key="tag.index" outline dense clickable color="grey" :selected=(doesKeyExist(tag)) :removable=(doesKeyExist(tag)) @remove="removeKey(tag)" @click="showKeyPrompt(tag)">{{ tag }}</q-chip>
         </div>
-        <div>
+        <div v-if="type !== 'jsonMeta'">
             Extra Props recognised by this site:
             <q-chip v-for="tag in myTags" :key="tag.index" outline dense clickable color="grey" :selected=(doesKeyExist(tag)) :removable=(doesKeyExist(tag)) @remove="removeKey(tag)" @click="showKeyPrompt(tag)">{{ tag }}</q-chip>
         </div>
         <json-viewer :data="JSON.parse(newjson)" />
         <q-dialog v-model="keyDialog" persistent>
-        <q-card style="min-width: 350px">
+        <q-card flat bordered style="min-width: 350px">
             <q-card-section>
             <div class="text-h6">Edit {{ keyDialogCurrentKey }}</div>
             </q-card-section>
@@ -35,7 +35,25 @@
 import jsonViewer from 'components/jsonViewer.vue'
 export default {
   name: 'propsEditor',
-  props: ['json', 'username', 'type'],
+  props: {
+    json: {
+      type: String,
+      required: false,
+      default: function () { return '{}' }
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    account: {
+      type: Object,
+      required: true
+    }
+  },
   components: { jsonViewer },
   data () {
     return {
@@ -50,30 +68,26 @@ export default {
       commonTags: ['name', 'about', 'location', 'profile_image', 'cover_image', 'website', 'witness_description', 'dtube_pub', 'pinned', 'portfolio', 'trail', 'default_page', 'default_view', 'witness_owner']
     }
   },
+  computed: {
+  },
   methods: {
     submitChanges () {
       if (this.type === 'postingMeta') {
-        var params = {
+        this.$store.commit('hive/addToQueue', [this.username, 'posting', ['account_update2', { account: this.username, posting_json_metadata: this.newjson }]])
+        this.$emit('editedProps')
+      }
+      if (this.type === 'jsonMeta' && this.account.posting && this.account.memo_key) {
+        var p = {
           account: this.username,
-          json_metadata: '',
-          posting_json_metadata: this.newjson
+          posting: this.account.posting,
+          memo_key: this.account.memo_key,
+          json_metadata: this.newjson
         }
-        var op = [
-          'account_update2',
-          params
-        ]
-        window.hive_keychain.requestBroadcast(this.username, [op], 'Posting', function (response, err) {
-          if (response.success === true) {
-            this.error = false
-            this.log = response.message
-            this.edited = true
-            this.$emit('editedProps')
-          }
-          if (response.success === false) {
-            this.error = true
-            this.log = response.message
-          }
-        }.bind(this))
+        console.log(p)
+        this.$store.commit('hive/addToQueue', [this.username, 'active', ['account_update', p]])
+        this.$emit('editedProps')
+      } else {
+        console.log('Error, cannot find info required to update JSON metadata')
       }
     },
     doesKeyExist (key) {
@@ -107,6 +121,8 @@ export default {
       this.keyDialog = true
     },
     mounted () {
+      console.log('json')
+      console.log(typeof (this.newjson))
     }
   }
 }
