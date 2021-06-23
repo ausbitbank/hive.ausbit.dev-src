4fcf8dfb8d2e2b88fcead01b091ea227400fd7e9
ausbitbank <ausbitbank@gmail.com>
2021-06-24 08:35:12 +1000

    witnesses.vue: Allow voting, proxying, viewing votes

--- src/components/witnesses.vue
+++ src/components/witnesses.vue
@@ -5,8 +5,19 @@
             <div class="text-h5">
                 <q-icon name="emoji_people" color="teal" /> Witnesses
             </div>
+            <div class="text-caption" v-if="loggedInUser && account !== undefined">
+              <div v-if="viewAll">
+                <q-btn flat dense label="My votes only"  @click="viewAll = false" color="primary" icon="face" v-if="viewAll" /><br />
+                Viewing all witnesses:
+              </div>
+              <div v-else>
+                <q-btn flat dense label="Show all"  @click="viewAll = true" color="primary" icon="clear_all" /><br />
+                <div v-if="account.proxy !== ''">Voting Proxy: <q-btn size="sm" flat dense icon-right="cancel" color="red" title="Remove voting proxy" :label="account.proxy" @click="setProxy('')" /></div>
+                Viewing my votes ({{ witnessVotes.length }}/30):
+              </div>
+            </div>
             <q-list separator dense>
-              <q-item v-for="(witness, rank) in witnesses" :key="witness.id">
+              <q-item v-for="(witness, rank) in witnessList" :key="witness.id">
                 <q-item-section avatar class="text-center">
                   <router-link class="text-primary" :to="linkAccount(witness.owner)">
                   <q-avatar size="xl">
@@ -25,7 +36,7 @@
                   </span>
                   <q-btn dense flat title="Click to show full witness properties">
                     <q-badge dense outline :color="getVersionColor(witness.running_version)">
-                      v{{ witness.running_version }}
+                      v{{ witness.running_version }} &nbsp; <q-icon color="info" size="xs" name="info" title="View more information, vote or unvote this witness" />
                     </q-badge>
                     <q-popup-proxy>
                       <q-card flat bordered>
@@ -36,6 +47,10 @@
                           <div v-if="witness.created !== '1970-01-01T00:00:00'">{{ witness.owner }} first registered their witness <b>{{ timeDelta(witness.created) }}</b></div>
                           <div v-else>{{ witness.owner }} has been a witness for <b>at least 5 years.</b></div>
                           <div v-if="loggedInUser === witness.owner"><q-btn flat icon="edit" color="orange" @click="$router.push('/witness')" label="Edit witness properties" /></div>
+                          <div v-if="loggedInUser && votingAccount && loggedInUser === votingAccount.name">
+                            <q-btn flat dense label="Unvote this witness" color="red" icon="cancel" @click="unvoteWitness(witness.owner)" v-if="witnessVotes.includes(witness.owner)" />
+                            <q-btn flat dense label="Vote this witness" color="orange" icon="how_to_vote" @click="voteWitness(witness.owner)" v-if="!witnessVotes.includes(witness.owner)" />
+                          </div>
                           <div><router-link :to="linkAccount(witness.owner)">Profile</router-link>, <router-link :to="linkAccountBlog(witness.owner)">Blog</router-link>, <router-link :to="linkAccountPosts(witness.owner)">Posts</router-link>, <router-link :to="linkAccountWallet(witness.owner)">Wallet</router-link></div>
                         </q-card-section>
                         <json-viewer :data="witness" class="q-ml-md" />
@@ -66,7 +81,39 @@
           <router-link to="witnesses" v-if="this.$route.path !== '/witnesses'"><q-btn dense flat icon="link" /></router-link>
           <q-btn dense flat icon="unfold_more" @click="limit = 200; witnesses = null; getWitnessesByVote()" v-if="limit < 200 "/>
           <q-btn dense flat icon="unfold_less" @click="limit = 25; witnesses = null; getWitnessesByVote()" v-if="limit === 200 "/>
+          <q-btn dense flat icon="more_horiz" v-if="loggedInUser">
+            <q-menu>
+              <q-list bordered separator>
+                <q-item clickable @click="proxyDialog = true" class="text-primary" title="Voting Proxy">
+                  <q-item-section avatar><q-icon name="add" /></q-item-section>
+                  <q-item-section>Voting Proxy</q-item-section>
+                </q-item>
+                <q-item clickable @click="init()" class="text-green" title="Refresh data" v-close-popup>
+                  <q-item-section avatar><q-icon name="refresh" /></q-item-section>
+                  <q-item-section>Refresh</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
         </q-card-actions>
+        <q-dialog v-model="proxyDialog">
+          <q-card flat bordered>
+            <q-card-section header class="text-h5 text-center">
+              Set a voting proxy
+            </q-card-section>
+            <q-card-section>
+              <div>
+                <q-icon name="info" color="info" size="md" style="float:left" class="q-ma-md" />
+                Enter a username here to select a proxy that will vote for witnesses and proposals on your behalf.
+              </div>
+              <q-input label="proxy account" v-model="newProxy" />
+            </q-card-section>
+            <q-card-actions class="text-center">
+              <q-btn flat label="Set Proxy" icon="edit" color="orange" @click="setProxy(newProxy)" v-close-popup />
+              <q-btn flat label="Cancel and close" icon="cancel" color="primary" v-close-popup/>
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
     </q-card>
     </span>
 </template>
@@ -78,6 +125,7 @@ a:visited { color: #884488; }
 <script>
 import moment from 'moment'
 import jsonViewer from 'components/jsonViewer.vue'
+import { debounce } from 'quasar'
 export default {
   name: 'witnesses',
   props: {
@@ -106,11 +154,68 @@ export default {
   data () {
     return {
       witnesses: null,
-      hardforkVersion: null
+      hardforkVersion: null,
+      viewAll: true,
+      proxyDialog: false,
+      newProxy: ''
+    }
+  },
+  watch: {
+    loggedInUser: function () { if (this.loggedInUser) { debounce(this.init(), 400) } },
+    account: function () {
+      if (this.account === undefined && this.loggedInUser) { debounce(this.$store.dispatch('hive/getAccount', this.loggedInUser), 500) }
+      if (this.account !== undefined) {
+        if (this.account.proxy !== '') { debounce(this.$store.dispatch('hive/getAccount', this.account.proxy), 500) }
+      }
     }
   },
   computed: {
-    loggedInUser: { get () { return this.$store.state.hive.user.username } }
+    loggedInUser: { get () { return this.$store.state.hive.user.username } },
+    account: {
+      get () {
+        if (this.loggedInUser) {
+          return this.$store.state.hive.accounts[this.loggedInUser]
+        } else {
+          return undefined
+        }
+      }
+    },
+    witnessVotes: {
+      get () {
+        if (this.loggedInUser && this.account !== undefined && this.votingAccount !== undefined) {
+          return this.votingAccount.witness_votes
+        } else {
+          return []
+        }
+      }
+    },
+    myWitnesses: {
+      get () {
+        if (this.loggedInUser && this.account !== undefined && this.witnesses.length > 0) {
+          return this.witnesses.filter(w => this.witnessVotes.includes(w.owner))
+        } else {
+          return []
+        }
+      }
+    },
+    witnessList: {
+      get () {
+        if (this.viewAll) { return this.witnesses } else { return this.myWitnesses }
+      }
+    },
+    votingAccount: {
+      get () {
+        if (this.account !== undefined) {
+          if (this.account.proxy === '') {
+            return this.account
+          } else {
+            return this.$store.state.hive.accounts[this.account.proxy]
+          }
+        } else {
+          return undefined
+        }
+      }
+    }
   },
   methods: {
     timeDelta (timestamp) {
@@ -123,45 +228,18 @@ export default {
       this.$hive.api.getWitnessesByVoteAsync('', this.limit)
         .then((response) => { this.witnesses = response })
     },
-    votesToHp (votes) {
-      return ((votes * this.hivePerMvests) / 1000000000000).toFixed(0)
-    },
-    getVersionColor (version) {
-      if (this.hardforkVersion) {
-        if (this.hardforkVersion.substr(0, 4) === version.substr(0, 4)) {
-          return 'green'
-        } else {
-          return 'grey'
-        }
-      } else {
-        return 'grey'
-      }
-    },
+    votesToHp (votes) { return ((votes * this.hivePerMvests) / 1000000000000).toFixed(0) },
+    getVersionColor (version) { if (this.hardforkVersion) { if (this.hardforkVersion.substr(0, 4) === version.substr(0, 4)) { return 'green' } else { return 'grey' } } else { return 'grey' } },
     getHardforkVersion () {
       this.$hive.api.getHardforkVersionAsync()
         .then((res) => { this.hardforkVersion = res })
     },
-    alertPricefeedAge (age) {
-      if (moment(age).isBefore(moment().subtract(1, 'd'))) {
-        return true
-      } else {
-        return false
-      }
-    },
-    alertSigningDisabled (key) {
-      if (key === 'STM1111111111111111111111111111111114T1Anm') {
-        return true
-      } else {
-        return false
-      }
-    },
-    alertNewAccount (created) {
-      if (moment(created).isAfter(moment().subtract(6, 'months'))) {
-        return true
-      } else {
-        return false
-      }
-    },
+    alertPricefeedAge (age) { if (moment(age).isBefore(moment().subtract(1, 'd'))) { return true } else { return false } },
+    alertSigningDisabled (key) { if (key === 'STM1111111111111111111111111111111114T1Anm') { return true } else { return false } },
+    alertNewAccount (created) { if (moment(created).isAfter(moment().subtract(6, 'months'))) { return true } else { return false } },
+    voteWitness (witness) { this.$store.commit('hive/addToQueue', [this.loggedInUser, 'active', ['account_witness_vote', { account: this.loggedInUser, witness: witness, approve: 'true' }]]) },
+    unvoteWitness (witness) { this.$store.commit('hive/addToQueue', [this.loggedInUser, 'active', ['account_witness_vote', { account: this.loggedInUser, witness: witness, approve: 'false' }]]) },
+    setProxy (proxy) { this.$store.commit('hive/addToQueue', [this.loggedInUser, 'active', ['account_witness_proxy', { account: this.loggedInUser, proxy: proxy }]]) },
     linkAccount (username) { return '/@' + username },
     linkAccountBlog (username) { return '/@' + username + '/blog' },
     linkAccountPosts (username) { return '/@' + username + '/posts' },
@@ -171,11 +249,20 @@ export default {
       var parts = x.toString().split('.')
       parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
       return parts.join('.')
+    },
+    init () {
+      this.getWitnessesByVote()
+      this.getHardforkVersion()
+      if (this.loggedInUser) { this.$store.dispatch('hive/getAccount', this.loggedInUser) }
+      if (this.account !== undefined) {
+        if (this.account.proxy !== '') {
+          this.$store.dispatch('hive/getAccount', this.account.proxy)
+        }
+      }
     }
   },
   mounted () {
-    this.getWitnessesByVote()
-    this.getHardforkVersion()
+    this.init()
   }
 }
 </script>
