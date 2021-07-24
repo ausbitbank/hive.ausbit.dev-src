fa50a4f49df449e39fc2cf4c50fd9f63611e29b8
ausbitbank <ausbitbank@gmail.com>
2021-07-24 19:50:24 +1000

    browseTable.vue: dense table

--- src/components/browseTable.vue
+++ src/components/browseTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-table :dense="$q.screen.lt.md || true" :grid="postTableGridStyle" label="Posts as table view" :data="filteredPosts" :columns="postTableColumns" :visible-columns="postTableColumnsVisible" :row-key="row => row.post_id" :pagination="postTablePagination" separator="cell" :loading="loading">
+  <q-table dense :grid="postTableGridStyle" label="Posts as table view" :data="filteredPosts" :columns="postTableColumns" :visible-columns="postTableColumnsVisible" :row-key="row => row.post_id" :pagination="postTablePagination" separator="cell" :loading="loading">
     <template v-slot:top>
       <q-space />
       <q-checkbox v-model="postTableGridStyle" label="Grid" class="q-mr-xs" />
