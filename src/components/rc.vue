0d95db0926aeb2cb4d84e2468c2f8ae4cf87c5b1
ausbitbank <ausbitbank@gmail.com>
2021-10-20 17:45:08 +1000

    rc.vue : Add account claim estimate, claiming tool

--- src/components/rc.vue
+++ src/components/rc.vue
@@ -1,15 +1,22 @@
 <template>
   <div v-if="RC.rc_manabar !== undefined">
-    <q-linear-progress dark stripe rounded size="20px" :value="rcPercent" color="green" class="q-mt-sm">
+    <q-linear-progress dark stripe rounded size="20px" :value="rcPercent" color="green" class="q-mt-sm" v-if="!showAccountClaimsOnly">
       <div class="absolute-full flex flex-center">
           <q-badge color="black" text-color="primary" >{{ (rcPercent * 100).toFixed(2) }} %</q-badge>
       </div>
     </q-linear-progress>
     <q-card-section class="text-center">
-        <div class="text-bold">Enough credits for approximately <q-icon name="info" color="blue" title="Click for more RC info" @click="showAll = !showAll" /></div>
-        <div>{{ resourceBudgetComments }} comments</div>
-        <div>{{ resourceBudgetVotes }} votes</div>
-        <div>{{ resourceBudgetTransfers }} transfers</div>
+        <div class="text-bold">Enough Resource Credits for approximately <q-icon name="info" color="blue" title="Click for more RC info" @click="showAll = !showAll" /></div>
+        <span v-if="!showAccountClaimsOnly">
+        <div><q-badge color="primary">{{ resourceBudgetComments }}</q-badge> comments</div>
+        <div><q-badge color="primary">{{ resourceBudgetVotes }}</q-badge> votes</div>
+        <div><q-badge color="primary">{{ resourceBudgetTransfers }}</q-badge> transfers</div>
+        </span>
+        <div><q-badge :color="resourceBudgetClaimAccounts > 0 ? 'primary' : 'red'">{{ resourceBudgetClaimAccounts }}</q-badge> account claims</div>
+    </q-card-section>
+    <q-card-section v-if="showAccountClaimsOnly">
+      <q-btn dense color="primary" icon="generating_tokens" label="Claim account creation token" @click="claimAccount()" v-if="loggedInUser" :disable="resourceBudgetClaimAccounts < 1" />
+      <q-btn dense flat color="primary" icon="refresh" @click="init()" />
     </q-card-section>
     <q-dialog v-model="showAll">
       <q-card flat bordered style="min-width: 300px text-center">
@@ -26,7 +33,7 @@
 // Calculate Resource Credits availability, present as a progress bar
 export default {
   name: 'rc',
-  props: ['username', 'display'],
+  props: ['username', 'display', 'showAccountClaimsOnly'],
   data () {
     return {
       loading: false,
@@ -39,6 +46,7 @@ export default {
     jsonViewer: () => import('components/jsonViewer.vue')
   },
   computed: {
+    loggedInUser: function () { return this.$store.state.hive.user.username },
     rcPercent: function () {
       if (this.RC.max_rc !== undefined) {
         var max = this.RC.max_rc
@@ -89,6 +97,20 @@ export default {
       } else {
         return null
       }
+    },
+    resourceBudgetClaimAccounts: function () {
+      if (this.RC.rc_manabar !== undefined) {
+        var cost = 8541343515163
+        if (this.costs !== null) { cost = this.costs.claim_account }
+        var available = (this.RC.rc_manabar.current_mana / cost)
+        if (available >= 1000000) {
+          return '1M+'
+        } else {
+          return this.tidyNumber(available.toFixed(0))
+        }
+      } else {
+        return null
+      }
     }
   },
   methods: {
@@ -113,11 +135,19 @@ export default {
       } else {
         return null
       }
+    },
+    claimAccount () {
+      var tx = ['claim_account', { creator: this.loggedInUser, fee: '0.000 HIVE', extensions: [] }]
+      this.$store.commit('hive/addToQueue', [this.loggedInUser, 'active', tx])
+      this.init()
+    },
+    init () {
+      this.getRcForAccount(this.username)
+      if (this.costs === null) { this.getResourceCosts() }
     }
   },
   mounted () {
-    this.getRcForAccount(this.username)
-    if (this.costs === null) { this.getResourceCosts() }
+    this.init()
   }
 }
 </script>
