6db4dc0f33b4891221670a9ec369b670b468eda0
ausbitbank <ausbitbank@gmail.com>
2021-11-02 16:25:03 +1000

    tipDialog.vue : Support new token format, icons, link to exchange

--- src/components/tipDialog.vue
+++ src/components/tipDialog.vue
@@ -55,18 +55,28 @@
         <q-item-label>
           {{ coin }}
         </q-item-label>
-        <q-item-label caption v-if="false">
-          <a :href="returnCoinLink(coin)" :title="accountMeta.profile[coin]">{{ accountMeta.profile[coin].substr(0,20) }}</a>..
-        </q-item-label>
       </q-item-section>
-      <q-item-section side v-if="false"> <!-- v-if="coin === 'ethereum'" -->
-        <!-- <metamask type='button' /> -->
-      </q-item-section>
-      <q-item-section side v-else>
+      <q-item-section side>
         <q-btn dense round type="a" :href="returnCoinLink(coin)" icon="send" title="Open Wallet Client" color="primary" class="hvr" />
       </q-item-section>
       <q-item-section side>
         <q-btn dense round icon="content_copy" color="blue-grey" title="Copy address to clipboard" @click="copy(accountMeta.profile[coin])" class="hvr"/>
+      </q-item-section>
+    </q-item>
+    <q-item clickable v-for="token in tokens" :key="token.index">
+      <q-item-section avatar>
+        <q-avatar size="lg"><q-img :src="getTokenImage(token)" :title="token" /></q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>
+          {{ token }}
+        </q-item-label>
+      </q-item-section>
+      <q-item-section side>
+        <q-btn dense round type="a" :href="getExchangeLink(account, token, accountMeta.profile.tokens[token])" icon="swap_vertical_circle" title="Exchange" color="orange" class="hvr" />
+      </q-item-section>
+      <q-item-section side>
+        <q-btn dense round icon="content_copy" color="blue-grey" title="Copy address to clipboard" @click="copy(accountMeta.profile.tokens[token])" class="hvr"/>
       </q-item-section>
     </q-item>
   </q-list>
@@ -82,6 +92,7 @@ export default {
   data () {
     return {
       supportedCoins: ['bitcoin', 'litecoin', 'ethereum', 'dogecoin', 'lightning'],
+      supportedTokens: [],
       hivetransfer: false,
       hivedollartransfer: false,
       ethFromAddress: null
@@ -111,8 +122,21 @@ export default {
       var userCoins = []
       if (this.accountMeta) {
         if ('profile' in this.accountMeta) {
-          Object.keys(this.accountMeta.profile).forEach(e => this.supportedCoins.includes(e) ? userCoins.push(e) : console.log())
+          Object.keys(this.accountMeta.profile).forEach(e => this.supportedCoins.includes(e) ? userCoins.push(e) : this.noop())
           return userCoins
+        } else {
+          return []
+        }
+      } else {
+        return []
+      }
+    },
+    tokens: function () {
+      var userTokens = []
+      if (this.accountMeta) {
+        if ('profile' in this.accountMeta) {
+          if ('tokens' in this.accountMeta.profile) { Object.keys(this.accountMeta.profile.tokens).forEach(e => this.supportedTokens.includes(e) ? userTokens.push(e) : this.noop()) }
+          return userTokens
         } else {
           return []
         }
@@ -131,13 +155,21 @@ export default {
     copy (input) {
       copyToClipboard(input)
       this.$q.notify('Copied to clipboard: ' + input)
-    }
+    },
+    getTokens () {
+      this.$axios.get('https://exchange-api.ausbit.dev/getCurrencies')
+        .then((res) => { this.supportedTokens = res.data.result.sort() })
+    },
+    getTokenImage (token) { return 'https://web-api.changelly.com/api/coins/' + token.toLowerCase() + '.png' },
+    getExchangeLink (username, token, address) { return 'https://hive.ausbit.dev/exchange?for=' + username + '&to=' + token + '&address=' + address },
+    noop () {}
   },
   mounted () {
     if (this.$store.state.hive.accounts[this.account] === undefined) {
       console.log('dispatch sent to get account info for ' + this.account)
       this.$store.dispatch('hive/getAccount', this.account)
     }
+    if (this.accountMeta.profile && this.accountMeta.profile.tokens) { this.getTokens() }
   }
 }
 </script>
