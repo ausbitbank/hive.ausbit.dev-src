1a35f1cd7cdff20896e06df0f5ebd5e5be735878
ausbitbank <ausbitbank@gmail.com>
2022-04-12 22:13:47 +1000

    settings.vue : dialog padding

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card flat bordered class="rounded-corners">
+  <q-card flat bordered class="rounded-corners q-pa-sm">
     <div class="text-h5 text-center">Settings</div>
       <q-list>
         <q-item>
