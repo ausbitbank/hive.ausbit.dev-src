71a2286b71e93f0c05d016f3a20bce6eb1caaa5c
ausbitbank <ausbitbank@gmail.com>
2022-04-19 00:00:18 +1000

    Update reblog.vue

--- src/components/reblog.vue
+++ src/components/reblog.vue
@@ -1,5 +1,17 @@
 <template>
-  <q-btn no-caps label="Reblog this post" :disabled="reblogged" dense push rounded flat size="md" @click="reblog(author, permlink)" class="capitalize" />
+  <q-btn no-caps label="Reblog this post" :disabled="reblogged" dense push rounded flat size="md" class="capitalize">
+    <q-popup-proxy>
+      <q-card flat bordered>
+        <q-card-section>
+          Are you sure you want to reblog this post ?
+        </q-card-section>
+        <q-card-actions>
+          <q-btn label="Yes" color="green" @click="reblog(author, permlink)" />
+          <q-btn label="No" color="red" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-popup-proxy>
+  </q-btn>
 </template>
 <script>
 // import { keychain } from '@hiveio/keychain'
