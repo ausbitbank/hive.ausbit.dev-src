6106ebc7ca8139cc8b3c794fcc2cbadcdcb7ead9
ausbitbank <ausbitbank@gmail.com>
2022-04-20 03:45:50 +1000

    Update animInt.vue

--- src/components/animInt.vue
+++ src/components/animInt.vue
@@ -8,7 +8,7 @@ export default {
   props: ['value'],
   components: {},
   watch: {
-    value () { gsap.to(this, { tweenValue: this.value, ease: 'Linear.easeNone', onUpdate: () => { this.displayValue = this.tweenValue } }) }
+    value () { gsap.to(this, { tweenValue: this.value, ease: 'elastic', onUpdate: () => { this.displayValue = this.tweenValue } }) }
   },
   methods: {
     tidyNumber (x) { if (x) { var parts = x.toString().split('.'); parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ','); return parts.join('.') } else { return null } }
