0c4d486cb725c2245eef8ea66539070f362da717
ausbitbank <ausbitbank@gmail.com>
2022-04-19 14:52:14 +1000

    Create animInt.vue

---
+++ src/components/animInt.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>{{ tidyNumber(displayValue) }}</span>
+</template>
+<script>
+import gsap from 'gsap'
+export default {
+  name: 'animInt',
+  props: ['value'],
+  components: {},
+  watch: {
+    value () { gsap.to(this, { tweenValue: this.value, ease: 'Linear.easeNone', onUpdate: () => { this.displayValue = this.tweenValue } }) }
+  },
+  methods: {
+    tidyNumber (x) { if (x) { var parts = x.toString().split('.'); parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ','); return parts.join('.') } else { return null } }
+  },
+  data () {
+    return {
+      displayValue: this.value,
+      tweenValue: this.value
+    }
+  }
+}
+</script>
